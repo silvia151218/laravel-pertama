f7746d4265ac10b7c86da7a69f39ac43d32a9f9d
silvia151218 <dewisilvia1512@gmail.com>
2021-03-30 13:10:18 +0700

    first commit

---
+++ app/Http/Controllers/CobaController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CobaController extends Controller
+{
+    public function index()
+    {
+        return 'test berhasil';
+    }
+
+    public function urutan($ke)
+    {
+        return view ('urutan', ['ke' =>$ke]);
+    }
+}
